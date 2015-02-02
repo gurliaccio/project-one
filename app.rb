d7783711da4a69ccb666f658fdc40b7498503bba
Michael Gurliaccio <gurliaccio@gmail.com>
2015-02-02 09:53:21 -0500

    cocktails

--- app.rb
+++ app.rb
@@ -1,25 +1,29 @@
 require 'sinatra'
-#require_relative './model/inventory'
+require_relative './model/inventory'
 
 get '/' do
+  @active = 'home'
   erb :home
 end
 
-get '/about' do
-  erb :about
-end
-
 get '/items' do
+  @active = 'items'
   @search = params[:search]
   if @search && @search.length > 0
     @items = Inventory.search(@search)
-  else 
+  else
     @items = Inventory.all
   end
   erb :items
 end
 
 get '/items/:id' do
+  @active = 'items'
   @item = Inventory.find(params[:id].to_i)
   erb :item
+end
+
+get '/profile' do
+  @active = 'about'
+  erb :about
 end
--- model/item.rb
+++ model/item.rb
@@ -1,17 +1,21 @@
 # This class represents a single Item you can purchase in the store.
 class Item
-  attr_reader :id, :category, :name, :price, :description
+  attr_reader :id, :category, :name, :price
+  attr_reader :ingredients, :description, :image_file
 
   def initialize(args)
     @id          = args[:id]
     @category    = args[:category]
     @name        = args[:name]
-    @price       = args[:price]
+    # @price       = args[:price]
+    @ingredients = args[:ingredients]
     @description = args[:description]
+    @image_file  = args[:image_file]
   end
 
   def to_s
     "ID: #{@id}, Category: #{@category}, Name: #{@name}, " \
-    "Price: #{@price}, Description: #{@description}"
+    "Ingredients: #{@ingredients}, Description: #{@description}, " \
+    "Image File: #{@image_file}"
   end
 end
